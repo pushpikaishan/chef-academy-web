3b947291180308af9ca31ccfea2f5e9cc93b0ff0
Pushpika Ishan <pushpikaishan@gmail.com>
2026-01-15 16:12:47 +0530

    Update api.js
    api uri

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,7 @@
+
 import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.MODE === 'production' ? 'https://chef-academy-web.onrender.com' : 'http://localhost:5000');
 
